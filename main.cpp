8196a6eb77fb26da1838c9ef0e7398e40eef50d4
ruijiehe <heruijie@stu.scu.edu.cn>
2025-06-20 15:33:27 +0800

    笔记

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
-#include "Bfs/nearestExit.h"
-int main()
+#include <iostream>
+#include <thread>
+#include <future>
+int plus(int left,int right) 
 {
-    std::unique_ptr<Solution> p = std::make_unique<Solution>();
-    p->test();
+    return left + right;
+}
+auto main() -> int
+{
+    std::promise<int> prom;
+    std::future<int> f = prom.get_future();
+    std::thread([&_prom = prom](int left,int right) mutable {
+        auto t = plus(left,right);
+        _prom.set_value(t);
+    }, 1, 2).join();
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    std::cout << f.get() << std::endl;
     return 0;
 }
-
-
