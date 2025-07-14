a549c106c979f0a0cd7f2720e23e92a5eac23e7e
ruijiehe <heruijie@stu.scu.edu.cn>
2025-07-14 11:12:58 +0800

    笔记提交

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "thread.hpp"
-
-
-auto main() -> int
-{
-    Thread([](int left, int right){ std::cout << "hello world" << std::endl; }, 1, 2 ).join();
-    return 0;
-}
